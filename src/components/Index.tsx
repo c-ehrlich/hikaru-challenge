6059a573c4d65467431676bc8505f14c1413dd6e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-30 22:51:58 +0100

    Add progress to Index

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,15 +1,23 @@
 import useStore from "../store"
+import styled from "styled-components";
+import hikaru from "../hikaru.json"
+
+const StyledIndex = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`;
 
 const Index = () => {
   const videoIndex = useStore(state => state.videoIndex);
   const setVideoIndex = useStore(state => state.setVideoIndex)
 
   return (
-    <div>
-      <div>{ videoIndex }</div>
-      <button onClick={() => {setVideoIndex(videoIndex+1)}}>+</button>
-      <button onClick={() => {setVideoIndex(videoIndex-1)}}>-</button>
-    </div>
+    <StyledIndex>
+      <button onClick={() => {setVideoIndex(videoIndex-1)}}>Previous</button>
+      <div>{videoIndex}/{hikaru.length}</div>
+      <button onClick={() => {setVideoIndex(videoIndex+1)}}>Next</button>
+    </StyledIndex>
   )
 }
 
