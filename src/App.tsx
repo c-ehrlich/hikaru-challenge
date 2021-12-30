1bcfea27b159907b2b850cf262188abc50547932
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-30 22:54:29 +0100

    create DOM structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Index from "./components/Index";
+import SetIndex from "./components/SetIndex";
+import Video from "./components/Video";
+
+const StyledApp = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  gap: 16px;
+`;
+
+const StyledAppTop = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp className="App">
+      <StyledAppTop>
+        <h1>Hikaru Challenge</h1>
+        <Video />
+        <Index />
+      </StyledAppTop>
+      <SetIndex />
+    </StyledApp>
   );
 }
 
